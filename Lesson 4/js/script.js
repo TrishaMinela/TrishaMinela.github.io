b5311aa9db5e864157a3dc6fb87572ba61a94b80
TrishaMinela <sumiran.trishaminela@gmail.com>
2021-05-15 05:23:24 +0800

    today

--- Lesson 4/js/script.js
+++ Lesson 4/js/script.js
@@ -1,9 +1,13 @@
-document.querySelector("#update").textContent = document.lastModified;
+function toggleMenu() {
+    document.getElementsByClassName("hamburger")[0].classList.toggle("responsive");
+}
 
 let date = new Date();
 let year = date.getFullYear();
 document.querySelector("#year").textContent = year;
 
-function toggleMenu() {
-    document.getElementsByClassName("hamburger")[0].classList.toggle("responsive");
-}
+
+let month = date.getMonth()+1;
+let day = date.getDate();
+let daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+document.querySelector("#current").textContent =  daylist[day] + ", " + day + " " + month + " " + year;
