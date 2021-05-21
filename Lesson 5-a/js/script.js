f9249fc6b01f6776318b56e58a700cee4ff2c550
TrishaMinela <sumiran.trishaminela@gmail.com>
2021-05-22 01:17:56 +0800

    commit

---
+++ Lesson 5-a/js/script.js
@@ -0,0 +1,21 @@
+function toggleMenu() {
+    document.getElementsByClassName("hamburger")[0].classList.toggle("responsive");
+}
+
+let date = new Date();
+let year = date.getFullYear();
+let day = date.getDay();
+let month = date.getMonth()+1;
+let daynumber = date.getDate();
+document.querySelector("#year").textContent = year;
+let daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+let nameday = daylist[day];
+let monthlist = ["January", "February", "March", "April" ,"May", "June", "July", "August", "September", "October", "November", "December"]
+let monthname = monthlist[month];
+document.querySelector("#current").textContent =  `${nameday}, ${daynumber} ${monthname} ${year}`;
+
+
+
+
+
+
