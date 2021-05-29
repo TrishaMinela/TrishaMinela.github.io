63fdfabaf21ee2b724a7f058ad1fab3565beb706
TrishaMinela <sumiran.trishaminela@gmail.com>
2021-05-30 01:45:17 +0800

    commit

--- Lesson 6/js/windchill.js
+++ Lesson 6/js/windchill.js
@@ -5,7 +5,7 @@ if(temperature <=10 && speed >= 4.8){
     let exponent = Math.pow(speed, 0.16);
     let windchill = 35.74 + 0.6215 * temperature - 35.75 * exponent + 0.4275 * temperature * exponent;
     let output = windchill.toFixed(0); 
-    document.getElementById('windchill').textContent = output + "&deg;C";  
+    document.getElementById('windchill').textContent = output + "&deg;" + "C";  
 }
 
 else{
