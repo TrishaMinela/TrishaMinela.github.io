97396a7e14397682e97b2aa08e6df1627c7851e1
TrishaMinela <sumiran.trishaminela@gmail.com>
2021-06-06 10:55:42 +0800

    change

--- Lesson-7/js/localstorage.js
+++ Lesson-7/js/localstorage.js
@@ -1,8 +1,9 @@
 // Local Storage
 
-var date1 = new Date(parseInt(localStorage.getItem[date1]));
-var date2 = new Date(parseInt(localStorage.getItem[date2]));
-var Timedif = date2 - date1;
-var Daydif = Math.round (Timedif / (1000 * 3600 * 24));
-document.querySelector("since").textContent = `Your last visit was: ${Daydif} days ago`;
-
+if (typeof Storage !== "undefined") {
+        var date1 = new Date(parseInt(localStorage.getItem[date1]));
+        var date2 = new Date(parseInt(localStorage.getItem[date2]));
+        var Timedif = date2 - date1;
+        var Daydif = Math.round (Timedif / (1000 * 3600 * 24));
+        document.querySelector("since").textContent = Daydif;
+    } 
