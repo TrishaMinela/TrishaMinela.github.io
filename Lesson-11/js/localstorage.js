1acbc010be08876bc0c8f02e410b270509c341f3
TrishaMinela <sumiran.trishaminela@gmail.com>
2021-07-04 01:57:56 +0800

    commit

--- Lesson-11/js/localstorage.js
+++ Lesson-11/js/localstorage.js
@@ -7,3 +7,72 @@ if (typeof Storage !== "undefined") {
         var Daydif = Math.round (Timedif / (1000 * 3600 * 24));
         document.querySelector("#since").textContent = Daydif;
     } 
+
+
+var load;
+window.onload = function() {
+
+  start = new Date();
+
+  var welcomeText;
+
+  if (typeof Storage !== "undefined") {
+
+      if (localStorage.visitcount && !isNaN(localStorage.visitcount)) {
+
+        var durationSinceLastVisit = start - new Date(localStorage.lastVisitDate);
+
+
+          var msPerDays= 24*60*60*1000;
+
+          var daysSinceLastVisit = Math.floor(durationSinceLastVisit/msPerDays);
+
+          var msPerMinutes= 60*1000;
+
+          var timeSpentInMinutes = Math.floor(localStorage.duration/msPerMinutes);
+
+          welcomeText = "Welcome back! Last visit was " + daysSinceLastVisit + " day(s) ago, "+timeSpentInMinutes + " minute(s) spent on this page during your " + localStorage.visitcount + " visits.";
+
+          
+      } else {
+
+         localStorage.visitcount = 1;
+
+         localStorage.duration = 0;
+
+         localStorage.lastVisitDate = start;
+
+         welcomeText = "This is your first time here! Welcome.";
+
+    }
+
+  } else {
+
+      welcomeText="Sorry, your browser does not support web storage...";
+
+ 
+
+  }
+
+\
+};
+
+ 
+
+window.onunload = function() {
+
+  var end = new Date();
+
+  var timeSpent = Math.abs(end - start);
+
+  if (typeof Storage !== "undefined") {
+
+    localStorage.visitcount = Number(localStorage.visitcount) + 1;
+
+    localStorage.duration = Number(localStorage.duration) + timeSpent;
+
+    localStorage.lastVisitDate = end;
+
+  }
+
+};
