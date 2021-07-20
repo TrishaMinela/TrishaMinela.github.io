430dc9c2f76ac5e5d3dac1cfb3edf7393df32deb
TrishaMinela <sumiran.trishaminela@gmail.com>
2021-07-21 01:14:09 +0800

    go

--- Lesson-12/javascript/weather.js
+++ Lesson-12/javascript/weather.js
@@ -1,21 +1,18 @@
-const apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=14.1384&lon=121.3198&appid=79cf3ff62a1685b8d326951e4140b61b";
 
-fetch(apiURL)
-  .then((response) => response.json())
-  .then((jsObject) => {
-    console.log(jsObject);
-    let day = 1;
-    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 
-   
-    apiURL.forEach( x => {
-        let date = new Date();
-        document.getElementById(`days${day}`).textContent = days[date.getDay()];
-        document.getElementById(`temp${day}`).textContent = `${current.temp}°F`;
-        document.getElementById(`description${day}`).textContent = `${current.weather.description}`;
-        document.getElementById(`humidity${day}`).textContent = `${current.humidity}`;
-        day++
-    })
-    })
+    const apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=14.1384&lon=121.3198&appid=79cf3ff62a1685b8d326951e4140b61b";
+    fetch(apiURL)
+      .then((response) => response.json())
+      .then((jsObject) => {
+        console.log(jsObject);
+        document.getElementById(`temp`).textContent = `${current.temp}°F`;
+        document.getElementById(`description`).textContent = `${current.weather.description}`;
+        document.getElementById(`humidity`).textContent = `${current.humidity}`;
+    });
+    
+    
+
+    
+
     
 
