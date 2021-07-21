a58a3bf73465b53d7e99794ff1f2e70f82d25339
TrishaMinela <sumiran.trishaminela@gmail.com>
2021-07-21 21:59:48 +0800

    go

--- Lesson-12/javascript/business.js
+++ Lesson-12/javascript/business.js
@@ -4,5 +4,40 @@ fetch("https://trishaminela.github.io/Lesson-12/json/directory.json")
     })
 
     .then(function(data){
-        
+        const businesses = data['businesses'];
+        const page = businesses.filter(specific => specific.BusinessName == "TJZED Cafe" || "Lucidel Resort" || "Morales Resort" || "Iskargu Restaurant" || "Isdaan Floating Restaurant" || "Sweet Tooth" || "The Wing Alley" || "ZeusPaulo Burger House")
+
+        page.forEach(businesses => {
+            let BusinessSpace = document.createElement('section');
+            BusinessSpace.className = "BusinessSpace"
+            let h2 = document.createElement('h2');
+            h2.className = "h2";
+            let p1 = document.createElement('p');
+            p1.className = "p1";
+            let p2 = document.createElement('p');
+            p2.className = "p2";
+            let logo = document.createElement('img');
+            logo.className = "logo";
+            let feature = document.createElement('img');;
+            feature.className = "feature";
+
+            h2.textContent = `${businesses.BusinessName}`;
+            BusinessSpace.appendChild(h2)
+
+            p1.textContent = `${businesses.ContactNumber}`;
+            BusinessSpace.appendChild(p1)
+
+            p2.textContent = `${businesses.Website}`;
+            BusinessSpace.appendChild(p2)
+
+            logo.setAttribute('src', businesses.Logo);
+            logo.setAttribute('alt', `The logo of: ${businesses.BusinessName}`);
+            BusinessSpace.appendChild(logo);
+
+            feature.setAttribute('src', businesses.FeatureImage);
+            feature.setAttribute('alt', `The image of: ${businesses.BusinessName}`);
+            BusinessSpace.appendChild(feature);
+
+            document.getElementById('businesses').appendChild(BusinessSpace)
+        })
     })
--- Lesson-12/javascript/weather.js
+++ Lesson-12/javascript/weather.js
@@ -1,18 +1,21 @@
+fetch("https://api.openweathermap.org/data/2.5/onecall?lat=14.1384&lon=121.3198&appid=79cf3ff62a1685b8d326951e4140b61b")
+  .then(function (response) {
+    return response.json();
+  })
 
+  .then(function(data){
+  
+    const weather = data['current'];
+    const page = weather.filter(specific => specific.timezone == "Asia/Manila")
 
-    const apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=14.1384&lon=121.3198&appid=79cf3ff62a1685b8d326951e4140b61b";
-    fetch(apiURL)
-      .then((response) => response.json())
-      .then((jsObject) => {
-        console.log(jsObject);
-        document.getElementById(`temp`).textContent = `${current.temp}°F`;
-        document.getElementById(`description`).textContent = `${current.weather.description}`;
-        document.getElementById(`humidity`).textContent = `${current.humidity}`;
-    });
+    weather.forEach(weather => {
+      document.getElementById(`temp`).textContent = `${weather.current.temp}`
+      document.getElementById(`description`).textContent = `${weather.current.weather.description}`
+      document.getElementById(`humidity`).textContent = `${weather.current.humidity}`
+
+    })
     
-    
+
+});
 
     
-
-    
-
