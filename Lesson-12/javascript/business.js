d223c23bb2aabab0f32ea9da035548f26ed34ea3
TrishaMinela <sumiran.trishaminela@gmail.com>
2021-07-21 18:59:34 +0800

    go

---
+++ Lesson-12/javascript/business.js
@@ -0,0 +1,8 @@
+fetch("https://trishaminela.github.io/Lesson-12/json/directory.json")
+    .then(function (response) {
+        return response.json();
+    })
+
+    .then(function(data){
+        
+    })
